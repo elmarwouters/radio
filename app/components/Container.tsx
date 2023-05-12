730e626de2d5c34603a611ff054d0ce8aa57ee74
Elmar Wouters <elmar@elmarwouters.nl>
2023-05-12 12:32:49 +0200

    Basic things

---
+++ app/components/Container.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type ContainerProps = {
+    children: React.ReactNode
+    className?: string
+}
+
+export const Container = ({children, className}: ContainerProps) => {
+    return (
+        <div className={`container ${className}`}>
+            {children}
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image'
+import Player from "@/components/Player";
+import {Container} from "@/components/Container";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="flex flex-col items-center justify-center">
+        <section>
+            <Container>
                 <h1 className="text-6xl font-bold text-center">
                     Hoi pipeloi!
                 </h1>
-            </div>
-        </main>
+                <Player/>
+            </Container>
+        </section>
     )
 }
